cedd12e72b0fa9c0f0e07fc138c66c7c5b4445b6
Frederic Bor <frederic.bor@wanadoo.fr>
2020-03-04 08:54:38 +0100

    pfsense_group: fix typos in example

--- library/pfsense_group.py
+++ library/pfsense_group.py
@@ -55,9 +55,10 @@ EXAMPLES = """
 - name: Add adservers group
   pfsense_group:
     name: Domain Admins
-    description: Remote Admins
+    descr: Remote Admins
     scope: remote
     priv: [ 'page-all', 'user-shell-access' ]
+    state: present
 
 - name: Remove group
   pfsense_group:
