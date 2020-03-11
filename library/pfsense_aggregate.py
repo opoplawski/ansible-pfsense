0112c51c419ef6a6b640bd8dc39684687bdca3fa
Frederic Bor <frederic.bor@wanadoo.fr>
2020-03-11 07:30:07 +0100

    pfsense_aggregate: interface_filter parameter
    it can be desirable to apply rules only on one interface,
    and to not apply/purge rules on the others. The interface_filter
    parameter is designed for such cases.

--- library/pfsense_aggregate.py
+++ library/pfsense_aggregate.py
@@ -335,6 +335,10 @@ options:
     required: False
     default: False
     type: bool
+  interface_filter:
+    description: only apply rules and rules separators on this interface
+    required: False
+    type: str
 """
 
 EXAMPLES = """
@@ -519,10 +523,30 @@ class PFSenseModuleAggregate(object):
                 return True
         return False
 
+    @staticmethod
+    def is_filtered(interface_filter, floating_filter, params):
+        if interface_filter is None:
+            return False
+
+        if 'floating' in params:
+            if isinstance(params['floating'], str):
+                floating = params['floating'].lower()
+            else:
+                floating = 'true' if params['floating'] else 'false'
+
+            if floating != 'false' and floating != 'no':
+                return not floating_filter
+
+        return floating_filter or params['interface'].lower() != interface_filter
+
     def run_rules(self):
         """ process input params to add/update/delete all rules """
 
         want = self.module.params['aggregated_rules']
+        interface_filter = self.module.params['interface_filter'].lower() if self.module.params.get('interface_filter') is not None else None
+        floating_filter = False
+        if interface_filter is not None and interface_filter.lower() == 'floating':
+            floating_filter = True
 
         if want is None:
             return
@@ -556,6 +580,8 @@ class PFSenseModuleAggregate(object):
                     todel.append(params)
 
             for params in todel:
+                if self.is_filtered(interface_filter, floating_filter, params):
+                    continue
                 self.pfsense_rules.run(params)
 
         # generating order if required
@@ -584,6 +610,8 @@ class PFSenseModuleAggregate(object):
 
         # processing aggregated parameters
         for params in want:
+            if self.is_filtered(interface_filter, floating_filter, params):
+                continue
             self.pfsense_rules.run(params)
 
     def run_aliases(self):
@@ -639,13 +667,19 @@ class PFSenseModuleAggregate(object):
     def run_rule_separators(self):
         """ process input params to add/update/delete all separators """
         want = self.module.params['aggregated_rule_separators']
+        interface_filter = self.module.params['interface_filter'].lower() if self.module.params.get('interface_filter') is not None else None
+        floating_filter = False
+        if interface_filter is not None and interface_filter.lower() == 'floating':
+            floating_filter = True
 
         if want is None:
             return
 
         # processing aggregated parameter
-        for param in want:
-            self.pfsense_rule_separators.run(param)
+        for params in want:
+            if self.is_filtered(interface_filter, floating_filter, params):
+                continue
+            self.pfsense_rule_separators.run(params)
 
         # delete every other if required
         if self.module.params['purge_rule_separators']:
@@ -665,6 +699,8 @@ class PFSenseModuleAggregate(object):
                         todel.append(params)
 
             for params in todel:
+                if self.is_filtered(interface_filter, floating_filter, params):
+                    continue
                 self.pfsense_rule_separators.run(params)
 
     def run_vlans(self):
@@ -732,6 +768,7 @@ def main():
         purge_rules=dict(default=False, type='bool'),
         purge_rule_separators=dict(default=False, type='bool'),
         purge_vlans=dict(default=False, type='bool'),
+        interface_filter=dict(required=False, type='str'),
     )
 
     required_one_of = [['aggregated_aliases', 'aggregated_interfaces', 'aggregated_rules', 'aggregated_rule_separators', 'aggregated_vlans']]
