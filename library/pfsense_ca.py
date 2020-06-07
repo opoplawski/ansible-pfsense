5e5528a44790c8e02b532a4e0a2562cdf6da8bf7
Orion Poplawski <orion@nwra.com>
2020-06-06 20:56:09 -0600

    [pfsense_ca] Convert to PFSenseModuleBase

--- library/pfsense_ca.py
+++ library/pfsense_ca.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# Copyright: (c) 2018-2019, Orion Poplawski <orion@nwra.com>
+# Copyright: (c) 2018-2020, Orion Poplawski <orion@nwra.com>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
@@ -28,7 +28,7 @@ options:
     type: str
   state:
     description: State in which to leave the Certificate Authority
-    required: true
+    default: present
     choices: [ "present", "absent" ]
     type: str
   certificate:
@@ -73,102 +73,166 @@ import base64
 import re
 
 from ansible.module_utils.basic import AnsibleModule
-from ansible.module_utils.network.pfsense.pfsense import PFSenseModule
+from ansible.module_utils.network.pfsense.module_base import PFSenseModuleBase
 
 
-class pfSenseCA(object):
+class PFSenseCAModule(PFSenseModuleBase):
+    """ module managing pfsense certificate authorities """
 
-    def __init__(self, module):
-        self.module = module
-        self.pfsense = PFSenseModule(module)
+    def __init__(self, module, pfsense=None):
+        super(PFSenseCAModule, self).__init__(module, pfsense)
+        self.name = "pfsense_ca"
+        self.root_elt = self.pfsense.root
         self.cas = self.pfsense.get_elements('ca')
-        self.crls = self.pfsense.get_elements('crl')
+        self.refresh_crls = False
 
-    def _find_ca(self, name):
-        found = None
-        i = 0
-        for ca in self.cas:
-            i = self.pfsense.get_index(ca)
-            if ca.find('descr').text == name:
-                found = ca
-                break
-        return (found, i)
+    ##############################
+    # params processing
+    #
+    def _validate_params(self):
+        """ do some extra checks on input parameters """
+        params = self.params
 
-    def _find_crl(self, caref):
-        found = None
-        i = 0
-        for crl in self.crls:
-            i = self.pfsense.get_index(crl)
-            if crl.find('caref').text == caref:
-                found = crl
-                break
-        return (found, i)
-
-    def validate_cert(self, cert):
         # TODO - Make sure certificate purpose includes CA
+        cert = params['certificate']
         lines = cert.splitlines()
         if lines[0] == '-----BEGIN CERTIFICATE-----' and lines[-1] == '-----END CERTIFICATE-----':
-            return base64.b64encode(cert)
-        elif re.match('LS0tLS1CRUdJTiBDRVJUSUZJQ0FURS0tLS0t', cert):
-            return cert
-        else:
+            params['certificate'] = base64.b64encode(cert.encode()).decode()
+        elif not re.match('LS0tLS1CRUdJTiBDRVJUSUZJQ0FURS0tLS0t', cert):
             self.module.fail_json(msg='Could not recognize certificate format: %s' % (cert))
 
-    def validate_crl(self, crl):
-        lines = crl.splitlines()
-        if lines[0] == '-----BEGIN X509 CRL-----' and lines[-1] == '-----END X509 CRL-----':
-            return base64.b64encode(crl)
-        elif re.match('LS0tLS1CRUdJTiBYNTA5IENSTC0tLS0t', crl):
-            return crl
-        else:
-            self.module.fail_json(msg='Could not recognize CRL format: %s' % (crl))
+        if params['crl'] is not None:
+            crl = params['crl']
+            lines = crl.splitlines()
+            if lines[0] == '-----BEGIN X509 CRL-----' and lines[-1] == '-----END X509 CRL-----':
+                params['crl'] = base64.b64encode(crl.encode()).decode()
+            elif not re.match('LS0tLS1CRUdJTiBYNTA5IENSTC0tLS0t', crl):
+                self.module.fail_json(msg='Could not recognize CRL format: %s' % (crl))
 
-    def add(self, ca):
-        ca_elt, ca_idx = self._find_ca(ca['descr'])
-        changed = False
+    def _params_to_obj(self):
+        """ return a dict from module params """
+        params = self.params
+
+        obj = dict()
+        self.obj = obj
+
+        obj['descr'] = params['name']
+        if params['state'] == 'present':
+            if 'certificate' in params and params['certificate'] is not None:
+                obj['crt'] = params['certificate']
+            if 'crl' in params and params['crl'] is not None:
+                obj['crl'] = params['crl']
+
+        return obj
+
+    ##############################
+    # XML processing
+    #
+    def _find_target(self):
+        result = self.root_elt.findall("ca[descr='{0}']".format(self.obj['descr']))
+        if len(result) == 1:
+            return result[0]
+        elif len(result) > 1:
+            self.module.fail_json(msg='Found multiple certificate authorities for name {0}.'.format(self.obj['descr']))
+        else:
+            return None
+
+    def _find_this_ca_index(self):
+        return self.cas.index(self.target_elt)
+
+    def _find_last_ca_index(self):
+        if len(self.cas):
+            return list(self.root_elt).index(self.cas[len(self.cas) - 1])
+        else:
+            return len(list(self.root_elt))
+
+    def _find_crl(self, caref):
+        result = self.root_elt.findall("crl[caref='{0}']".format(caref))
+        if len(result) == 1:
+            return result[0]
+        elif len(result) > 1:
+            self.module.fail_json(msg='Found multiple CRLs for caref {0}.'.format(caref))
+        else:
+            return None
+
+    def _create_target(self):
+        """ create the XML target_elt """
+        return self.pfsense.new_element('ca')
+
+    def _copy_and_add_target(self):
+        """ populate the XML target_elt """
+        obj = self.obj
         crl = {}
-        diff = {}
-        stdout = None
-        stderr = None
-        if 'crl' in ca:
+        if 'crl' in obj:
             crl['method'] = 'existing'
-            crl['text'] = ca.pop('crl')
-        if ca_elt is None:
-            diff['before'] = ''
-            changed = True
-            ca_elt = self.pfsense.new_element('ca')
-            ca['refid'] = self.pfsense.uniqid()
-            self.pfsense.copy_dict_to_element(ca, ca_elt)
-            self.pfsense.root.append(ca_elt)
-            if 'text' in crl:
+            crl['text'] = obj.pop('crl')
+
+        obj['refid'] = self.pfsense.uniqid()
+        self.pfsense.copy_dict_to_element(obj, self.target_elt)
+        self.diff['after'] = self.pfsense.element_to_dict(self.target_elt)
+        self.root_elt.insert(self._find_last_ca_index(), self.target_elt)
+        if 'text' in crl:
+            crl_elt = self.pfsense.new_element('crl')
+            crl['refid'] = self.pfsense.uniqid()
+            crl['descr'] = obj['descr'] + ' CRL'
+            crl['caref'] = obj['refid']
+            self.pfsense.copy_dict_to_element(crl, crl_elt)
+            self.diff['after']['crl'] = crl['text']
+            self.pfsense.root.append(crl_elt)
+            self.refresh_crls = True
+
+    def _copy_and_update_target(self):
+        """ update the XML target_elt """
+        obj = self.obj
+        before = self.pfsense.element_to_dict(self.target_elt)
+        self.diff['before'] = before
+
+        crl = {}
+        if 'crl' in obj:
+            crl['method'] = 'existing'
+            crl['text'] = obj.pop('crl')
+
+        changed = self.pfsense.copy_dict_to_element(obj, self.target_elt)
+        self.diff['after'] = self.pfsense.element_to_dict(self.target_elt)
+
+        if 'text' in crl:
+            crl_elt = self._find_crl(self.target_elt.find('refid').text)
+            if crl_elt is None:
+                changed = True
                 crl_elt = self.pfsense.new_element('crl')
                 crl['refid'] = self.pfsense.uniqid()
-                crl['descr'] = ca['descr'] + ' CRL'
-                crl['caref'] = ca['refid']
+                crl['descr'] = obj['descr'] + ' CRL'
+                crl['caref'] = self.target_elt.find('refid').text
                 self.pfsense.copy_dict_to_element(crl, crl_elt)
-                self.pfsense.root.append(crl_elt)
-            descr = 'ansible pfsense_ca added %s' % (ca['descr'])
-        else:
-            diff['before'] = self.pfsense.element_to_dict(ca_elt)
-            if 'text' in crl:
-                crl_elt, crl_index = self._find_crl(ca_elt.find('refid').text)
-                if crl_elt is None:
+                # Add after the existing ca entry
+                self.pfsense.root.insert(self._find_this_ca_index() + 1, crl_elt)
+                self.refresh_crls = True
+            else:
+                before['crl'] = crl_elt.find('text').text
+                if self.pfsense.copy_dict_to_element(crl, crl_elt):
                     changed = True
-                    crl_elt = self.pfsense.new_element('crl')
-                    crl['refid'] = self.pfsense.uniqid()
-                    crl['descr'] = ca['descr'] + ' CRL'
-                    crl['caref'] = ca_elt.find('refid').text
-                    self.pfsense.copy_dict_to_element(crl, crl_elt)
-                    # Add after the existing ca entry
-                    self.pfsense.root.insert(ca_idx + 1, crl_elt)
-                else:
-                    diff['before']['crl'] = crl_elt.find('text').text
-                    changed = self.pfsense.copy_dict_to_element(crl, crl_elt)
-            if self.pfsense.copy_dict_to_element(ca, ca_elt):
-                changed = True
-            descr = 'ansible pfsense_ca updated "%s"' % (ca['descr'])
-        if changed and not self.module.check_mode:
-            self.pfsense.write_config(descr=descr)
+                    self.refresh_crls = True
+            self.diff['after']['crl'] = crl['text']
+
+        return (before, changed)
+
+    ##############################
+    # Logging
+    #
+    def _get_obj_name(self):
+        """ return obj's name """
+        return self.obj['descr']
+
+    def _log_fields(self, before=None):
+        """ generate pseudo-CLI command fields parameters to create an obj """
+        values = ''
+        return values
+
+    ##############################
+    # run
+    #
+    def _update(self):
+        if self.params['state'] == 'present':
             # ca_import will base64 encode the cert + key  and will fix 'caref' for CAs that reference each other
             # $ca needs to be an existing reference (particularly 'refid' must be set) before calling ca_import
             # key and serial are optional arguments.  TODO - handle key and serial
@@ -178,37 +242,33 @@ class pfSenseCA(object):
                 ca_import($ca, '{cert}');
                 print_r($ca);
                 print_r($config['ca']);
-                write_config();""".format(refid=ca_elt.find('refid').text, cert=base64.b64decode(ca_elt.find('crt').text)))
-            if 'text' in crl:
-                self.pfsense.phpshell("""
+                write_config();""".format(refid=self.target_elt.find('refid').text, cert=base64.b64decode(self.target_elt.find('crt').text.encode()).decode()))
+
+            crl_stdout = ''
+            crl_stderr = ''
+            if self.refresh_crls:
+                (dummy, crl_stdout, crl_stderr) = self.pfsense.phpshell("""
                     require_once("openvpn.inc");
                     openvpn_refresh_crls();
                     require_once("vpn.inc");
                     vpn_ipsec_configure();""")
+                return (dummy, stdout + crl_stdout, stderr + crl_stderr)
 
-        diff['after'] = self.pfsense.element_to_dict(ca_elt)
-        if 'text' in crl:
-            diff['after']['crl'] = crl['text']
-        self.module.exit_json(changed=changed, diff=diff, stdout=stdout, stderr=stderr)
-
-    def remove(self, ca):
-        ca_elt, dummy = self._find_ca(ca['descr'])
-        changed = False
-        diff = {}
-        diff['after'] = {}
-        if ca_elt is not None:
-            changed = True
-            diff['before'] = self.pfsense.element_to_dict(ca_elt)
-            crl_elt, dummy = self._find_crl(ca_elt.find('refid').text)
-            self.cas.remove(ca_elt)
-            if crl_elt is not None:
-                diff['before']['crl'] = crl_elt.find('text').text
-                self.crls.remove(crl_elt)
+            return (dummy, stdout + crl_stdout, stderr + crl_stderr)
         else:
-            diff['before'] = {}
-        if changed and not self.module.check_mode:
-            self.pfsense.write_config(descr='ansible pfsense_ca removed "%s"' % (ca['descr']))
-        self.module.exit_json(changed=changed, diff=diff)
+            return (None, '', '')
+
+    def _pre_remove_target_elt(self):
+        self.diff['after'] = {}
+        if self.target_elt is not None:
+            self.diff['before'] = self.pfsense.element_to_dict(self.target_elt)
+            crl_elt = self._find_crl(self.target_elt.find('refid').text)
+            self.cas.remove(self.target_elt)
+            if crl_elt is not None:
+                self.diff['before']['crl'] = crl_elt.find('text').text
+                self.root_elt.remove(crl_elt)
+        else:
+            self.diff['before'] = {}
 
 
 def main():
@@ -216,29 +276,21 @@ def main():
         argument_spec={
             'name': {'required': True, 'type': 'str'},
             'state': {
-                'required': True,
+                'type': 'str',
+                'default': 'present',
                 'choices': ['present', 'absent']
             },
-            'certificate': {'required': False, 'type': 'str'},
-            'crl': {'required': False, 'default': None, 'type': 'str'},
+            'certificate': {'type': 'str'},
+            'crl': {'default': None, 'type': 'str'},
         },
         required_if=[
             ["state", "present", ["certificate"]],
         ],
         supports_check_mode=True)
 
-    pfca = pfSenseCA(module)
-
-    ca = dict()
-    ca['descr'] = module.params['name']
-    state = module.params['state']
-    if state == 'absent':
-        pfca.remove(ca)
-    elif state == 'present':
-        ca['crt'] = pfca.validate_cert(module.params['certificate'])
-        if module.params['crl'] is not None:
-            ca['crl'] = pfca.validate_crl(module.params['crl'])
-        pfca.add(ca)
+    pfmodule = PFSenseCAModule(module)
+    pfmodule.run(module.params)
+    pfmodule.commit_changes()
 
 
 if __name__ == '__main__':
